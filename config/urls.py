428ae57eecec1a02739bcac91159ed2d61101065
Sören Grieger <soeren.grieger@posteo.de>
2023-02-16 20:50:51 +0100

    created admin.py

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 ]"""
 
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     path(r'', include('api.urls'), name='main'),
     path(r'admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
