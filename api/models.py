6ebcef81f4ff42746b4e9c5adc2abefdd38017ab
Sören Grieger <soeren.grieger@posteo.de>
2023-02-17 09:09:18 +0100

    fixed user model to create user and superuser without username

--- api/models.py
+++ api/models.py
@@ -1,10 +1,80 @@
 import uuid
 
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import ArrayField
 from location_field.models.plain import PlainLocationField
 
 from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(
+        self,
+        email,
+        password,
+        first_name="",
+        last_name="",
+        username="",
+        **extra_fields
+    ):
+        """
+        Create and save a user with the given username, email, password, first_name, last_name and personal_number.
+        """
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(
+        self,
+        email="",
+        first_name="",
+        last_name="",
+        password="",
+        username="",
+        **extra_fields
+    ):
+        if not email:
+            raise ValueError("The field 'email' is required.")
+        if not first_name:
+            raise ValueError("The field 'first_name' is required.")
+        if not last_name:
+            raise ValueError("The field 'last_name' is required.")
+        if not password:
+            raise ValueError("The field 'password' is required.")
+        # We always set the provided username to the user's email
+        username = email
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(
+            username=username,
+            email=email,
+            password=password,
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields
+        )
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -13,7 +83,7 @@ class User(AbstractUser):
     id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True
     )
-    username = models.CharField(max_length=151, blank=True)
+    username = None
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)  # Firstname is required
     last_name = models.CharField(max_length=100)  # Lastname is required
@@ -25,6 +95,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
 
 
 class Building(models.Model):
@@ -99,7 +171,7 @@ class Booking(models.Model):
         primary_key=True, default=uuid.uuid4, editable=False, unique=True
     )
     workplaces = models.ManyToManyField(to=Workplace)
-    # users
+    user = models.ForeignKey(to=User, related_name="bookings", on_delete=models.CASCADE)
     started = models.DateTimeField()
     stopped = models.DateTimeField()
     email_others = ArrayField(base_field=models.CharField(max_length=250))
