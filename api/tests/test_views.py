ccb0bb53f8758cf9b0ff94f89e7803ef87eada7d
Sören Grieger <soeren.grieger@posteo.de>
2023-04-13 12:59:51 +0200

    testing readonly for building view

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -8,14 +8,6 @@ from api.models import Booking, Building, Room, User, Workplace
 
 class TestBuildingAPiEndpoint:
     @pytest.mark.django_db
-    def test_read_only(
-        self,
-        client,
-        user_object_jwt,
-    ):
-        pass
-
-    @pytest.mark.django_db
     def test_get_forbidden_without_jwt(self, client, building_object):
         """
         Test the detail Endpoint returns a 401 if no JWT is present.
@@ -73,6 +65,100 @@ class TestBuildingAPiEndpoint:
             content_type="application/json",
         )
         assert response.status_code == 401
+
+    @pytest.mark.django_db
+    def test_get_allowed(self, client, building_object, user_object_jwt):
+        """
+        Test the detail Endpoint returns a 200 if valid JWT is present.
+        :param client:
+        :param building_object:
+        :return:
+        """
+        client.credentials(
+            HTTP_AUTHORIZATION="Bearer {}".format(user_object_jwt)
+        )
+        response = client.get(
+            path=r"/buildings/",
+            args=[building_object.id],
+            content_type="application/json",
+        )
+        assert response.status_code == 200
+
+    @pytest.mark.django_db
+    def test_list_allowed(self, client, user_object_jwt):
+        """
+        Test the list endpoint returns a 200 if valid JWT is present.
+        :param client:
+        :return:
+        """
+        client.credentials(
+            HTTP_AUTHORIZATION="Bearer {}".format(user_object_jwt)
+        )
+        response = client.get(
+            path="http://localhost:8000/buildings/",
+            content_type="application/json",
+        )
+        assert response.status_code == 200
+
+    @pytest.mark.django_db
+    def test_put_not_allowed(
+        self, client, valid_building_json, user_object_jwt
+    ):
+        """
+        Test the PUT endpoint returns 405 if valid JWT is present.
+        :param client:
+        :param valid_building_json:
+        :return:
+        """
+        client.credentials(
+            HTTP_AUTHORIZATION="Bearer {}".format(user_object_jwt)
+        )
+        response = client.put(
+            path="http://localhost:8000/buildings/",
+            data=json.dumps(valid_building_json),
+            content_type="application/json",
+        )
+        assert response.status_code == 405
+
+    @pytest.mark.django_db
+    def test_patch_not_allowed(
+        self, client, valid_building_json, user_object_jwt
+    ):
+        """
+        Test the PATCH endpoint returns 405 if valid JWT is present.
+        :param client:
+        :param valid_building_json:
+        :return:
+        """
+        client.credentials(
+            HTTP_AUTHORIZATION="Bearer {}".format(user_object_jwt)
+        )
+        response = client.patch(
+            path="http://localhost:8000/buildings/",
+            data=json.dumps(valid_building_json),
+            content_type="application/json",
+        )
+        assert response.status_code == 405
+
+    @pytest.mark.django_db
+    def test_post_not_allowed(
+        self, client, valid_building_json, user_object_jwt
+    ):
+        """
+        Test the POST endpoint returns 405 if valid JWT is present.
+        :param client:
+        :param valid_building_json:
+        :return:
+        """
+        client.credentials(
+            HTTP_AUTHORIZATION="Bearer {}".format(user_object_jwt)
+        )
+        response = client.post(
+            path="http://localhost:8000/buildings/",
+            data=json.dumps(valid_building_json),
+            content_type="application/json",
+        )
+        assert response.status_code == 405
 
 
 class TestDjoserCustomizing:
